16e38bc2aaf438e68547952473ef48a603e57657
kelvin3kalu <kalukelvin2024@gmail.com>
2025-06-11 15:13:48 +0100

    add requirements

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 from .models import Product  # Correct the import path
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class ProductModelTest(TestCase):
@@ -12,3 +14,21 @@ class ProductModelTest(TestCase):
         self.assertEqual(product.name, "Test Product")
         self.assertEqual(product.price, 1000)
         self.assertEqual(product.description, "A sample test product")
+
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username = "kelvin"
+        self.password = 'nnnn4444'
+        self.user = User.objects.create_user(username= self.username,password=self.password)
+    def test_login_success(self):
+        response = self.client.post(reverse('login'),{
+            'username':self.username,
+            'password':self.password
+        })
+    def test_faliure_success(self):
+        response = self.client.post(reverse('login'),{
+            'username':self.username,
+            'password':'wrong password'
+        })         
