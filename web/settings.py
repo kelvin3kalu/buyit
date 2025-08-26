8a60766da94592f70d63450bc630e005a8307959
kelvin3kalu <kalukelvin2024@gmail.com>
2025-08-26 17:04:44 +0100

    new chnge

--- web/settings.py
+++ web/settings.py
@@ -80,11 +80,7 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.parse(config("DATABASE_URL"))
 }
 
 
